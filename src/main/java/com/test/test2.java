2239cd46a6eaae0f2f65a3309007f3990bdc886e
jzzzy <116899967+jzzzy@users.noreply.github.com>
2022-10-31 21:06:57 +0800

    Update test2.java
    123

--- src/main/java/com/test/test2.java
+++ src/main/java/com/test/test2.java
@@ -5,5 +5,6 @@ public class test2 {
         System.out.println("hello world2");
         System.out.println("hello world2");
         System.out.println("hello world33");
+        System.out.println("NB");
     }
 }
